6785aa1802c3b9dc8402a5738f05f7e83ac96e6b
Chris Diehl <cultclassik@gmail.com>
2019-01-09 21:44:50 -0600

    update syntax

--- manifests/config.pp
+++ manifests/config.pp
@@ -7,10 +7,10 @@
 class swarm_monitor::config {
 
   docker::stack { $::swarm_monitor::stack_name :
-    ensure       => present,
-    stack_name   => $::swarm_monitor::stack_name,
-    compose_file => $::swarm_monitor::compose_file,
-    require      => [
+    ensure        => present,
+    stack_name    => $::swarm_monitor::stack_name,
+    compose_files => [$::swarm_monitor::compose_file],
+    require       => [
       File[$::swarm_monitor::compose_file],
       File[$::swarm_monitor::grafana_volume],
     ],
